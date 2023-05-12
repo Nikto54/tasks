68747a5e383a500afb88c165659fc98fb107e0f7
Bogdan <bondarbm03@gmail.com>
2023-05-12 16:43:13 +0300

    init naval_battle

---
+++ naval battle.py
@@ -0,0 +1,15 @@
+class Dot:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=x
+
+class Ship():
+    def __int__(self,len,x,y,direction,lives):
+        self.len=len
+        self.x=x
+        self.y=y
+        self.direction=direction
+        self.lives=lives
+
+    def dots(self):
+
